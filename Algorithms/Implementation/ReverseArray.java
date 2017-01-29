80921431c56755cf8ea835c110d7034ef1e06e34
Badrinath Bhalkiker <Badrinath_Bhalkiker@gap.com>
2017-01-28 18:23:01 -0800

    Reverse an Array

---
+++ Algorithms/Implementation/ReverseArray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ReverseArray{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for (int i=0; i<n ; i++ ) {
+			a[i] = sc.nextInt();
+		}
+		for (int j = n-1; j>=0 ; j-- ) {
+			System.out.print(a[j]);
+		}
+	}
+}
