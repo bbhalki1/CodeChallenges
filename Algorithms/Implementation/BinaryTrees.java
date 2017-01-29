4706adc05f01e7719af0bf171bad854b548091bd
Badrinath Bhalkiker <Badrinath_Bhalkiker@gap.com>
2017-01-28 16:13:19 -0800

    Updating the Tree;

--- Algorithms/Implementation/BinaryTrees.java
+++ Algorithms/Implementation/BinaryTrees.java
@@ -1,23 +1,11 @@
 // Ref : cslibrary.stanford.edu
 
-public class BinaryTree { 
-  // Root node pointer. Will be null for an empty tree. 
+public class BinaryTrees { 
   private Node root; 
- 
-
-  /* 
-   --Node-- 
-   The binary tree is built using this nested node class. 
-   Each node stores one data element, and has left and right 
-   sub-tree pointer which may be null. 
-   The node is a "dumb" nested class -- we just use it for 
-   storage; it does not have any methods. 
-  */ 
   private static class Node { 
     Node left; 
     Node right; 
     int data;
-
     Node(int newData) { 
       left = null; 
       right = null; 
@@ -25,32 +13,16 @@ public class BinaryTree {
     } 
   }
 
-  /** 
-  * Creates an empty binary tree -- a null root pointer. 
-  */ 
   public void BinaryTree() { 
     root = null; 
   } 
- 
-
-  /** 
-  * Returns true if the given target is in the binary tree. 
-  * Uses a recursive helper. 
-  */ 
   public boolean lookup(int data) { 
     return(lookup(root, data)); 
   } 
- 
-
-  /** 
-  * Recursive lookup  -- given a node, recur 
-  * down searching for the given data. 
-  */ 
   private boolean lookup(Node node, int data) { 
-    if (node==null) { 
+      if (node==null) { 
       return(false); 
     }
-
     if (data==node.data) { 
       return(true); 
     } 
@@ -61,23 +33,10 @@ public class BinaryTree {
       return(lookup(node.right, data)); 
     } 
   } 
- 
-
-  /** 
-  * Inserts the given data into the binary tree. 
-  * Uses a recursive helper. 
-  */ 
   public void insert(int data) { 
     root = insert(root, data); 
   } 
  
-
-  /** 
-  * Recursive insert -- given a node pointer, recur down and 
-  * insert the given data into the tree. Returns the new 
-  * node pointer (the standard way to communicate 
-  * a changed pointer back to the caller). 
-  */ 
   private Node insert(Node node, int data) { 
     if (node==null) { 
       node = new Node(data); 
@@ -90,7 +49,6 @@ public class BinaryTree {
         node.right = insert(node.right, data); 
       } 
     }
-
-    return(node); // in any case, return the new pointer to the caller 
+    return(node);
   } 
- 
+ }
