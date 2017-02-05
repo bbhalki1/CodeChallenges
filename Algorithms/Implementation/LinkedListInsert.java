2cb7b5076422358f1d433e197e6bb608480a6ec7
Badrinath Bhalkiker <Badrinath_Bhalkiker@gap.com>
2017-02-04 16:17:16 -0800

    Insert at the tail

---
+++ Algorithms/Implementation/LinkedListInsert.java
@@ -0,0 +1,25 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+   if(head == null){
+       head = new Node();  // new node if thereis nothing
+       head.data = data;
+   }else{
+       Node node = head;
+       while (node.next != null){ // traverse till last node
+            node = node.next;
+       }
+       node.next = new Node(); // tail node
+       node.next.data = data; // tail data
+   }
+    return head;
+}
